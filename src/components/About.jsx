c6c3b62b5750bd42b82803b8e3ede71b19c22400
Prabhat-Kumar-0 <pbt8112001@gmail.com>
2024-08-24 21:09:12 +0530

    Fixed About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
   return (
-    <div className="text-zinc-300 bg-[#1F1E24] h-[110vh] w-[100vw] p-5">
+    <div className="text-zinc-300 bg-[#1F1E24] h-[100vh] w-[100vw] p-5">
       <h1 className="text-xl">About Binge Box</h1>
 
       <h3 className="mt-5">
@@ -19,47 +19,44 @@ function About() {
       </p>
 
       <h2 className="mt-5">Key Features</h2>
-      <h3 className="mt-3">1. Infinite Scroll:</h3>
-      <p>
-        Explore an extensive collection of movies, web series, and actors with
-        our infinite scroll feature. Keep scrolling to uncover more content and
-        information without any interruptions.
-      </p>
+      <li>
+        Infinite Scroll : Explore an extensive collection of movies, web series,
+        and actors with our infinite scroll feature. Keep scrolling to uncover
+        more content and information without any interruptions.
+      </li>
 
-      <h3 className="mt-3">2. Advanced Filtering Options:</h3>
-      <p>
-        Narrow down your search with our advanced filtering options. Filter by
-        genres, release dates, and more to find exactly what you're
-        looking for—be it a movie, TV show, or actor.
-      </p>
+      <li>
+        Advanced Filtering Options : Narrow down your search with our advanced
+        filtering options. Filter by genres, release dates, and more to find
+        exactly what you're looking for—be it a movie, TV show, or actor.
+      </li>
 
-      <h3 className="mt-3">3. Powerful Search Functionality:</h3>
-      <p>
-        Looking for specific details? Our robust search feature lets you quickly
-        find information on any movie, series, or actor . Just
-        enter the title, keywords, or name, and we’ll deliver the details you
-        need.
-      </p>
+      <li>
+        Powerful Search Functionality : Looking for specific details? Our robust
+        search feature lets you quickly find information on any movie, series,
+        or actor . Just enter the title, keywords, or name, and we’ll deliver
+        the details you need.
+      </li>
 
-      <h3 className="mt-3">4. Comprehensive Movie, Series, and Actor Details:</h3>
-      <p>
-        Binge Box offers in-depth information on movies, TV shows, and actors.
-        From plot summaries and cast details to ratings and actor biographies,
-        you’ll find everything you need to know in one place.
-      </p>
+      {/* <li>
+        Comprehensive Movie, Series, and Actor Details:Binge Box offers in-depth
+        information on movies, TV shows, and actors. From plot summaries and
+        cast details to ratings and actor biographies, you’ll find everything
+        you need to know in one place.
+      </li>
 
-      <h3 className="mt-3">5. Redux-Powered State Management:</h3>
-      <p>
-        Enjoy a smooth and responsive user experience thanks to Redux, which
-        efficiently manages state across our platform.
-      </p>
+      <li>
+        Redux-Powered State Management: Enjoy a smooth and responsive user
+        experience thanks to Redux, which efficiently manages state across our
+        platform.
+      </li>
 
-      <h3 className="mt-3">6. Modern Design with Tailwind CSS:</h3>
-      <p>
-        Navigate through our sleek and intuitive interface, designed with
-        Tailwind CSS. Our modern design ensures that exploring your favorite
-        movies, series, and actors is as enjoyable as it is informative.
-      </p>
+      <li>
+        Modern Design with Tailwind CSS: Navigate through our sleek and
+        intuitive interface, designed with Tailwind CSS. Our modern design
+        ensures that exploring your favorite movies, series, and actors is as
+        enjoyable as it is informative.
+      </li> */}
 
       <h2 className="mt-5">Technologies Behind Binge Box</h2>
       <h3>
@@ -71,8 +68,6 @@ function About() {
       <li>Redux: To manage application state seamlessly.</li>
       <li>Tailwind CSS: For a clean, modern, and responsive design.</li>
       <li>Vercel: For hosting Binge Box with optimal performance and speed.</li>
-
-     
     </div>
   );
 }
